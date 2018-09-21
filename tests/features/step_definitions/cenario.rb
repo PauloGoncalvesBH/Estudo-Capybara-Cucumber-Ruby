84f27e0159346250d5d6f634a835870c4963e13c
Paulo Gonçalves <paulorochag@hotmail.com>
2018-09-21 00:04:53 -0300

    Aula 17 - Inclusão de funcionalidade com mais de 1 cenário

---
+++ tests/features/step_definitions/cenario.rb
@@ -0,0 +1,21 @@
+Dado("eu tenho {int} laranjas.") do |valor1|
+    @laranjas = valor1
+end
+  
+Quando("eu como {int} laranjas.") do |valor2|
+    @comer = valor2
+    @resultado = @laranjas -@comer
+end
+  
+Então("eu vejo quantas laranjas sobraram.") do
+    expect(@resultado).to eq 8
+end
+
+Quando("eu compro {int} laranjas.") do |valor4|
+    @comprar = valor4
+    @compratotal = @laranjas + @comprar
+end
+  
+Então("eu vejo quantas laranjas eu tenho.") do
+    expect(@compratotal).to eq 15
+end
